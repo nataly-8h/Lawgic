daf4a17c29389eb27c6471a908f0640a85274c3a
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2021-11-02 16:18:06 -0600

    Template basico

--- lawgic-app/src/app/app.component.ts
+++ lawgic-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector   : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lawgic-app';
+export class AppComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
 }
