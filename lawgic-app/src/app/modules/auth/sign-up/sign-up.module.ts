c70172a990f8b74fe8b651b199eb27f9099626d9
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2021-11-14 23:03:16 -0600

    Login

--- lawgic-app/src/app/modules/auth/sign-up/sign-up.module.ts
+++ lawgic-app/src/app/modules/auth/sign-up/sign-up.module.ts
@@ -11,6 +11,7 @@ import { FuseAlertModule } from '@fuse/components/alert';
 import { SharedModule } from 'app/shared/shared.module';
 import { AuthSignUpComponent } from 'app/modules/auth/sign-up/sign-up.component';
 import { authSignupRoutes } from 'app/modules/auth/sign-up/sign-up.routing';
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
 
 @NgModule({
     declarations: [
