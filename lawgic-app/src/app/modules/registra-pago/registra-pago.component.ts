7c09906aa7249872e7240170af264c156a1dad02
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2021-12-09 12:30:47 -0600

    Seleccion de estados y com municipio terminado

--- lawgic-app/src/app/modules/registra-pago/registra-pago.component.ts
+++ lawgic-app/src/app/modules/registra-pago/registra-pago.component.ts
@@ -1,7 +1,7 @@
 // import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { fuseAnimations } from '@fuse/animations';
+// import { fuseAnimations } from '@fuse/animations';
 import { FuseAlertType } from '@fuse/components/alert';
 import { AuthService } from 'app/core/auth/auth.service';
 import  *  as  estado_data  from  './estados.json';
@@ -26,13 +26,15 @@ interface Food {
 })
 export class RegistraPagoComponent implements OnInit {
 
-  selected = 'option2';
-
+  selected = '';
+  selectedmun = '';
+  
   
   estados: any[] = new Array(32);
+  municipios: any[] = new Array();
+
 
   
-
   // let jsonCountries = JSON.stringify
 
   @ViewChild('signUpNgForm') signUpNgForm: NgForm;
@@ -62,7 +64,6 @@ export class RegistraPagoComponent implements OnInit {
     for(let i = 0; i < 32; i++){
       this.estados[i] = estado_data[i];
     }
-
 
   }
 
@@ -130,6 +131,155 @@ export class RegistraPagoComponent implements OnInit {
           this.showAlert = true;
         }
       );
+  }
+
+  cambiaEstado(): void{
+    let estnum:number;
+
+    switch(this.selected) { 
+      case 'AGS': { 
+         estnum = 0;
+         break; 
+      } 
+      case 'BC': { 
+        estnum = 1;
+         break; 
+      } 
+      case 'BCS': { 
+        estnum = 2;
+         break; 
+      } 
+      case 'CHI': { 
+        estnum = 3;
+         break; 
+      } 
+      case 'CHS': { 
+        estnum = 4;
+         break; 
+      } 
+      case 'CMP': { 
+        estnum = 5;
+         break; 
+      } 
+      case 'CMX': { 
+        estnum = 6;
+         break; 
+      } 
+      case 'COA': { 
+        estnum = 7;
+         break; 
+      } 
+      case 'COL': { 
+        estnum = 8;
+         break; 
+      } 
+      case 'DGO': { 
+        estnum = 9;
+         break; 
+      } 
+      case 'GRO': { 
+        estnum = 10;
+         break; 
+      } 
+      case 'GTO': { 
+        estnum = 11;
+         break; 
+      } 
+      case 'HGO': { 
+        estnum = 12;
+         break; 
+      } 
+      case 'JAL': { 
+        estnum = 13;
+         break; 
+      } 
+      case 'MCH': { 
+        estnum = 14;
+         break; 
+      } 
+      case 'MEX': { 
+        estnum = 15;
+         break; 
+      } 
+      case 'MOR': { 
+        estnum = 16;
+         break; 
+      } 
+      case 'NAY': { 
+        estnum = 17;
+         break; 
+      } 
+      case 'NL': { 
+        estnum = 18;
+         break; 
+      } 
+      case 'OAX': { 
+        estnum = 19;
+         break; 
+      } 
+      case 'PUE': { 
+        estnum = 20;
+         break; 
+      } 
+      case 'QR': { 
+        estnum = 21;
+         break; 
+      } 
+      case 'QRO': { 
+        estnum = 22;
+         break; 
+      } 
+      case 'SIN': { 
+        estnum = 23;
+         break; 
+      } 
+      case 'SLP': { 
+        estnum = 24;
+         break; 
+      } 
+      case 'SON': { 
+        estnum = 25;
+         break; 
+      } 
+      case 'TAB': { 
+        estnum = 26;
+         break; 
+      } 
+      case 'TLX': { 
+        estnum = 27;
+         break; 
+      } 
+      case 'TMS': { 
+        estnum = 28;
+         break; 
+      } 
+      case 'VER': { 
+        estnum = 29;
+         break; 
+      } 
+      case 'YUC': { 
+        estnum = 30;
+         break; 
+      } 
+      case 'ZAC': { 
+        estnum = 31;
+         break; 
+      } 
+      
+      default: { 
+         //statements; 
+         break; 
+      } 
+   } 
+
+    // if(this.selected === 'AGS'){
+
+    // }
+    this.municipios = new Array(this.estados[estnum].muni.length);
+    for(let i = 0; i < this.municipios.length; i++){
+      this.municipios[i] = this.estados[estnum].muni[i];
+    }
+    
   }
 
 }
